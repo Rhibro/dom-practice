a9c650e4d6e54f109cce7cf10a036c9bd10e4b95
Rhibro <rhibron@gmail.com>
2024-11-07 09:09:45 +0100

    added more övningar

--- Event-Management/event.js
+++ Event-Management/event.js
@@ -30,3 +30,42 @@ let btnOne = document.querySelector('button');
 
 btnOne.style.backgroundColor = '#2A527E';
 btnOne.style.color = 'black';
+
+// 6
+
+let backgrdColor = document.querySelectorAll('article');
+if (backgrdColor[2]){
+    backgrdColor[2].style.backgroundColor = '#2A527E';
+}
+
+// 7
+
+let switchAddress = document.querySelectorAll('p');
+if (switchAddress[3]) {
+    switchAddress[3].textContent = 'Main Street 14  \n 123 45, Happy Town';
+}
+
+// 8
+
+// let switchcolor = document.querySelectorAll('p');
+// if (switchcolor[0] && switchcolor[1] && switchcolor[2] && switchcolor[3]) {
+//     switchcolor[0].style.color = 'blue';
+//     switchcolor[1].style.color = 'blue';
+//     switchcolor[2].style.color = 'blue';
+//     switchcolor[3].style.color = 'blue';
+// } 
+
+let switchcolor = document.querySelectorAll('p');
+for (let i = 0; i < switchcolor.length; i++) {
+    switchcolor[i].style.color = 'blue';
+}
+
+// 9
+
+let btnColour = document.querySelectorAll('button');
+for (let i = 0; i < btnColour.length; i++) {
+    btnColour[i].textContent = 'Add to cart';
+}
+
+// 10
+
